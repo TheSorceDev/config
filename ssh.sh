1e9a2854e08cc28a92bebf5bb635461d96838a90
TheSorceDev <143894183+TheSorceDev@users.noreply.github.com>
2025-07-28 22:51:03 +0200

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,5 +1,1 @@
-mkdir -p ~/.ssh && echo "b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
-QyNTUxOQAAACAPImmmislTsfgH+wy20C5wOgRhP3hQ1JZ3CiP1bf4UCwAAAKDAu6t0wLur
-dAAAAAtzc2gtZWQyNTUxOQAAACAPImmmislTsfgH+wy20C5wOgRhP3hQ1JZ3CiP1bf4UCw
-AAAEBNph26CMM8CNwiIF1xhjAmLwg9urFJBmVfrbq74iQ/mA8iaaaKyVOx+Af7DLbQLnA6
-BGE/eFDUlncKI/Vt/hQLAAAAFmZ2b2Vsa2VsMjAxMEBnbWFpbC5jb20BAgMEBQYH" >> ~/.ssh/authorized_keys && chmod 700 ~/.ssh && chmod 600 ~/.ssh/authorized_keys
+mkdir -p ~/.ssh && echo "b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZWQyNTUxOQAAACAPImmmislTsfgH+wy20C5wOgRhP3hQ1JZ3CiP1bf4UCwAAAKDAu6t0wLurdAAAAAtzc2gtZWQyNTUxOQAAACAPImmmislTsfgH+wy20C5wOgRhP3hQ1JZ3CiP1bf4UCwAAAEBNph26CMM8CNwiIF1xhjAmLwg9urFJBmVfrbq74iQ/mA8iaaaKyVOx+Af7DLbQLnA6BGE/eFDUlncKI/Vt/hQLAAAAFmZ2b2Vsa2VsMjAxMEBnbWFpbC5jb20BAgMEBQYH" >> ~/.ssh/authorized_keys && chmod 700 ~/.ssh && chmod 600 ~/.ssh/authorized_keys
