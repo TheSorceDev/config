68a6fb770b57f40887cbf9e3dbd5ebf151a3c821
TheSorceDev <143894183+TheSorceDev@users.noreply.github.com>
2025-07-28 23:03:08 +0200

    Update steam.sh

--- steam.sh
+++ steam.sh
@@ -1,3 +1,3 @@
 read -sp "Enter your sudo password: " PASS
 echo
-echo "$PASS" | sudo -S pacman -Syu --noconfirm steam nvidia nvidia-utils lib32-nvidia-utils lib32-mesa lib32-vulkan-icd-loader
+echo "$PASS" | sudo -S pacman -Syu steam nvidia nvidia-utils lib32-nvidia-utils lib32-mesa lib32-vulkan-icd-loader
