5a60444a1666a271ed245cf8c740beead798acf0
TheSorceDev <143894183+TheSorceDev@users.noreply.github.com>
2025-07-28 23:05:09 +0200

    Update steam.sh

--- steam.sh
+++ steam.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
 read -sp "Enter your sudo password: " PASS
 echo
-echo "$PASS" | sudo -S pacman -Syu steam nvidia nvidia-utils lib32-nvidia-utils lib32-mesa lib32-vulkan-icd-loader
+
+echo "Installing Steam and NVIDIA drivers..."
+sudo pacman -Syu --noconfirm steam nvidia nvidia-utils lib32-nvidia-utils lib32-mesa lib32-vulkan-icd-loader
